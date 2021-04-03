c7e89716e3bd588e8e10306c926ae08690d47a41
matvb <mateus.villasboas@gmail.com>
2021-04-03 09:35:08 -0300

    Forth Class

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,5 +1,19 @@
+const Agendamento = require('../models/Agendamento');
+
 module.exports = app => {
     app.get('/agendamentos', (req, resp) => {
-        resp.send('Servidor OK')
+        Agendamento.listagem(resp);
     });
+
+    app.get('/agendamentos/:id', (req, resp) => {
+        const id = parseInd(req.params.id);
+        Agendamento.buscaPorId(id, resp);
+    });
+
+    app.post('/agendamentos', (req, resp) => {
+        const agendamento = req.body;
+        Agendamento.inserir(agendamento, resp);
+    });
+
+    
 };
